0646690d8cdb39381e7e1a00659296eca6aeac43
2hyunjinn <huenjin04@naver.com>
2025-05-17 21:26:24 +0900

    [init] #2 enum 생성

---
+++ src/main/java/org/sopt/sopkathon/domain/Region.java
@@ -0,0 +1,11 @@
+package org.sopt.sopkathon.domain;
+
+public enum Region {
+    GONGJOO("공주");
+
+    private final String description;
+
+    Region(String description) {
+        this.description = description;
+    }
+}
