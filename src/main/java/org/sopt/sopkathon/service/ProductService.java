80d9fffa7040c7f9faa5304f3a59aa861fc7daba
2hyunjinn <huenjin04@naver.com>
2025-05-17 23:47:44 +0900

    [FEAT] #11 상품 상세조회 API 기능 구현

---
+++ src/main/java/org/sopt/sopkathon/service/ProductService.java
@@ -0,0 +1,25 @@
+package org.sopt.sopkathon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.sopkathon.domain.ProductEntity;
+import org.sopt.sopkathon.dto.response.ProductDetailDto;
+import org.sopt.sopkathon.global.error.ErrorCode;
+import org.sopt.sopkathon.global.error.exception.BusinessException;
+import org.sopt.sopkathon.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDetailDto getProductDetail(Long productId) {
+        ProductEntity product = productRepository.findById(productId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.NO_SUCH_PRODUCT_ERROR));
+        return ProductDetailDto.of(product);
+    }
+
+}
