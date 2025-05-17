04c8c5c48b1eaffcf4e1d8b6d02753a0772178ca
seongmin0229 <leesung2925@gmail.com>
2025-05-18 01:07:07 +0900

    [FEAT] #16 카테고리, 지역 enum 설정

--- src/main/java/org/sopt/sopkathon/domain/Category.java
+++ src/main/java/org/sopt/sopkathon/domain/Category.java
@@ -4,7 +4,20 @@ import org.sopt.sopkathon.global.error.ErrorCode;
 import org.sopt.sopkathon.global.error.exception.BusinessException;
 
 public enum Category {
-    STRAWBERRY("딸기");
+    // 채소
+    CHILI("고추"),
+    GINSENG("인삼"),
+    SHIITAKE("표고"),
+    GARLIC("마늘"),
+
+    // 과일
+    APPLE("사과"),
+    STRAWBERRY("딸기"),
+    CHESTNUT("알밤"),
+    MELON("메론"),
+
+    // 곡물
+    RICE("쌀");
 
     private final String description;
 
