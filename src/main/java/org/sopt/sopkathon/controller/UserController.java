544199950a4ff1a27884df89c0d5f8737a85d5ed
2hyunjinn <huenjin04@naver.com>
2025-05-18 04:56:13 +0900

    [HOTFIX]

--- src/main/java/org/sopt/sopkathon/controller/UserController.java
+++ src/main/java/org/sopt/sopkathon/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
 
     @GetMapping("/{userId}")
