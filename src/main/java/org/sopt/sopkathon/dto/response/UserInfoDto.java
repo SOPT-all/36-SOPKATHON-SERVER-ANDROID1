574d81262cb16550e7de6fa9b24c746b4faaabcd
seongmin0229 <leesung2925@gmail.com>
2025-05-18 02:28:38 +0900

    [HOTFIX] #27 유저 이름 정보 누락 수정

--- src/main/java/org/sopt/sopkathon/dto/response/UserInfoDto.java
+++ src/main/java/org/sopt/sopkathon/dto/response/UserInfoDto.java
@@ -2,11 +2,12 @@ package org.sopt.sopkathon.dto.response;
 
 import org.sopt.sopkathon.domain.UserEntity;
 
-public record UserInfoDto(Long id, int totalPrice, int level, int maxPrice) {
+public record UserInfoDto(Long id, String username, int totalPrice, int level, int maxPrice) {
 
     public static UserInfoDto from(UserEntity user) {
         return new UserInfoDto(
                 user.getId(),
+                user.getName(),
                 user.getTotalPrice(),
                 user.getLevel().getLevel(),
                 user.getLevel().getMaxPrice()
