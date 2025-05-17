f768c9dbf9aaec25c36a5cc10c8092b940fa2f32
2hyunjinn <huenjin04@naver.com>
2025-05-18 00:27:54 +0900

    [FEAT] #15 유저 조회 기능 구현

--- src/main/java/org/sopt/sopkathon/service/UserService.java
+++ src/main/java/org/sopt/sopkathon/service/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
 
         return UserInfoDto.from(user);
     }
+
+    public UserEntity getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(NoSuchUserException::new);
+    }
 }
